8028e55f607892edff595ad3c40e5d511de27b76
Esteban Perez <esteban.perez@wsu.edu>
2026-03-11 22:22:53 +0000

    finished Tictactoe

--- tic-tac-toe/scripts.js
+++ tic-tac-toe/scripts.js
@@ -1,8 +1,8 @@
 let tictactoe = [
-[ "", "", "" ],
-[ "", "", "" ],
-[ "", "", "" ]
-]
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+];
 
 // track the turn
 let currentTurn = "x";
@@ -15,7 +15,7 @@ let currentPlayer;
 
 
 // Funstion to check the gameboard for a win or draw
-  function checkGameboard(tictactoe) {
+function checkGameboard(tictactoe) {
 
   //rows
   for (let row = 0; row < 3; row++) {
@@ -55,65 +55,91 @@ let currentPlayer;
     tictactoe[1][1] === tictactoe[2][0]
   ) {
     return tictactoe[0][2];
-  }else if ((winState =="d") && (remainingTurns == 0)) {
+  }
 
-  return "draw";
-}
+  // check for draw
+  if (remainingTurns == 0) {
+    return "draw";
+  }
+
+  return null;
 }
 
 //Function to handle clicks
 function clickSquare() {
 
-    // check if space is empty
-    if (this.innerHTML == "") {
-    
+  // check if space is empty
+  if (this.innerHTML == "") {
 
-        //set Space
-        this.innerHTML = currentTurn;
 
-        remainingTurns = remainingTurns - 1;
-        console.log("Remaining turns:" + remainingTurns);
+    //set Space
+    this.innerHTML = currentTurn;
 
-        //update array of rows with player value
-        if (this.id =="a1") tictactoe[0][0] = currentTurn;
-        if (this.id =="a2") tictactoe[0][1] = currentTurn;
-        if (this.id =="a3") tictactoe[0][2] = currentTurn;
-        if (this.id =="b1") tictactoe[1][0] = currentTurn;
-        if (this.id =="b2") tictactoe[1][1] = currentTurn;
-        if (this.id =="b3") tictactoe[1][2] = currentTurn;
-        if (this.id =="c1") tictactoe[2][0] = currentTurn;
-        if (this.id =="c2") tictactoe[2][1] = currentTurn;
-        if (this.id =="c3") tictactoe[2][2] = currentTurn;
+    remainingTurns = remainingTurns - 1;
+    console.log("Remaining turns:" + remainingTurns);
 
-        //output arrays to console
-        console.log(tictactoe[0]);
-        console.log(tictactoe[1]);
-        console.log(tictactoe[2]);
+    //update array of rows with player value
+    if (this.id == "a1") tictactoe[0][0] = currentTurn;
+    if (this.id == "a2") tictactoe[0][1] = currentTurn;
+    if (this.id == "a3") tictactoe[0][2] = currentTurn;
+    if (this.id == "b1") tictactoe[1][0] = currentTurn;
+    if (this.id == "b2") tictactoe[1][1] = currentTurn;
+    if (this.id == "b3") tictactoe[1][2] = currentTurn;
+    if (this.id == "c1") tictactoe[2][0] = currentTurn;
+    if (this.id == "c2") tictactoe[2][1] = currentTurn;
+    if (this.id == "c3") tictactoe[2][2] = currentTurn;
 
-        //flip between turns
-        if (currentTurn == "x") currentTurn = "o";
-        else currentTurn = "x";
+    //output arrays to console
+    console.log(tictactoe);
 
-        
 
-        currentPlayer.innerHTML = currentTurn;
+    let gameOutputMsg = document.querySelector("#gameResult");
+
+    // call your function checkGameboard() with the 3 rows
+
+    winState = checkGameboard(tictactoe)
+
+
+    // test the returned value of the function
+    if (winState == "x") {
+      gameOutputMsg.innerHTML = "X wins";
+
+    } else if (winState == "o") {
+      gameOutputMsg.innerHTML = "O wins";
+
+    } else if (winState == "d") {
+      gameOutputMsg.innerHTML = "draw";
+
+    } else {
+      gameOutputMsg.innerHTML = "unknown";
     }
+
+
+
+    //flip between turns
+    if (currentTurn == "x") currentTurn = "o";
+    else currentTurn = "x";
+
+
+
+    currentPlayer.innerHTML = currentTurn;
+  }
 }
 
 
 
 //waits for document to load
-    document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    let allSpace = document.querySelectorAll(".box");
+  let allSpace = document.querySelectorAll(".box");
 
-    // loops through all the clickables
-    for (let eachSpace of allSpace) {
-        eachSpace.addEventListener("click", clickSquare);  
-}
-    currentPlayer = document.querySelector("#currentPlayer span");
-    currentPlayer.innerHTML = currentTurn;
-});    
+  // loops through all the clickables
+  for (let eachSpace of allSpace) {
+    eachSpace.addEventListener("click", clickSquare);
+  }
+  currentPlayer = document.querySelector("#currentPlayer span");
+  currentPlayer.innerHTML = currentTurn;
+});
 
 
 /*
